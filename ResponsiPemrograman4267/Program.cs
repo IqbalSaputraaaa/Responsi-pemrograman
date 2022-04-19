f5d26bb8b09afd7688e1a676c5ccab4e6c1d4d5e
IqbalSaputraaaa <iqbalsaputra@students.amikom.ac.id>
2022-04-19 16:43:19 +0700

    first commit

---
+++ ResponsiPemrograman4267/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4267
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var krywn1 = new karyawan(190302123, "Paijo", 3000000);
+            var krywn2 = new karyawan(190302124, "Jono", 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); krywn1.tampilkanData();
+            Console.Write("2 "); krywn2.tampilkanData();
+            Console.WriteLine();
+
+            krywn1.naikGaji();
+            krywn2.naikGaji();
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); krywn1.tampilkanData();
+            Console.Write("2 "); krywn2.tampilkanData();
+
+        }
+    }
+}
---
+++ ResponsiPemrograman4267/karyawan.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4267
+{
+    class karyawan
+    {
+        private int nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public int Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+
+        public karyawan(int nik, string nama, int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            this.gajiBulanan = gajiBulanan < 0 ? 0 : gajiBulanan;
+        }
+
+        public void tampilkanData()
+        {
+            Console.WriteLine("{0} {1}      {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            int kenaikan;
+
+            kenaikan = gajiBulanan * 110;
+            gajiBulanan = kenaikan / 100;
+
+            Console.WriteLine("asyik naik gaji 10%");
+
+        }
+    }
+}
